81ad439898d48af9ac6f178ec884f66da0ced4da
fran <fran@mac.local>
2022-09-01 12:57:03 +0100

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { theme } from '@chakra-ui/pro-theme'
+import '@fontsource/inter/variable.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// const colors = {
+//   brand: {
+//     900: '#1a365d',
+//     800: '#153e75',
+//     700: '#2a69ac',
+//   },
+// }
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const myTheme = extendTheme(
+    {
+      colors: { ...theme.colors },
+    },
+    theme,
+  )
+  return (
+    <ChakraProvider theme={myTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
