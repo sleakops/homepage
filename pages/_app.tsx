865c342ecb6c9a88d7508c5f00eb025d0106e18a
Matias <matiroson@gmail.com>
2022-09-30 22:05:52 -0300

    fix: title and description tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,26 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import Head from "next/head";
 import { theme } from "@chakra-ui/pro-theme";
 
 import "@fontsource/inter/variable.css";
 
-
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  
-  const myTheme = extendTheme(
-    theme,
-    {
-      colors: { ...theme.colors },
-      config: {
-        ...theme.config,
-        initialColorMode: 'dark',
-        useSystemColorMode: false,
-      } 
+  const myTheme = extendTheme(theme, {
+    colors: { ...theme.colors },
+    config: {
+      ...theme.config,
+      initialColorMode: "dark",
+      useSystemColorMode: false,
     },
-    
-  );
+  });
 
   return (
     <ChakraProvider theme={myTheme}>
+      <Head>
+        <title>SleakOps - Deploy and Automate your Infra</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
