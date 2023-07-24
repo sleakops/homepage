2abfbb0add81cbbdc347f8dbf7dd6e7176861e9d
fran <fran@mac-2.local>
2023-07-24 15:53:34 -0300

    change calendly url

--- components/calendly_button/CalendlyButton.tsx
+++ components/calendly_button/CalendlyButton.tsx
@@ -41,7 +41,7 @@ export const Calendly = (
         </Button>
         <Box display="none">
           <PopupButton
-            url="https://calendly.com/fran-sleakops/demo-sleakops"
+            url="https://calendly.com/d/385-7np-2t6/demo"
             /*
              * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
              * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
